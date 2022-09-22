562d2c4029bb84dade7392517f4d237a6af7facd
whit3Q <gw1221@naver.com>
2022-09-22 19:26:58 +0900

    PRO12906

--- src/PRO12906/Solution.java
+++ src/PRO12906/Solution.java
@@ -4,21 +4,16 @@ import java.util.ArrayList;
 
 public class Solution {
     public int[] solution(int[] arr) {
-        int[] answer = {};
+
         ArrayList<Integer> result = new ArrayList<>();
-        if(arr.length > 0) {
-            int tmp = arr[0];
-            result.add(tmp);
-            for(int now : arr) {
-                if(tmp == now) {
-                    continue;
-                } else {
-                    result.add(now);
-                    tmp = now;
-                }
+        int tmp = 10;
+
+        for (int now : arr) {
+            if (tmp != now) {
+                result.add(now);
+                tmp = now;
             }
         }
-
 
         return result.stream().mapToInt(i -> i).toArray();
     }
